cd93822c94d8efe68ee68b1d18d4d53bc2c38997
Garcez0506 <fernando.garcez@acad.pucrs.br>
2022-08-19 16:19:46 -0300

    Criei o Codigo

--- Projeto/index.js
+++ Projeto/index.js
@@ -1,2 +1,8 @@
 import chalk from 'chalk';
-import readlineSync from 'readline-sync'
+import readlineSync from 'readline-sync';
+
+let quem = readlineSync.question ('Quem quer comprar?');
+let oQue = readlineSync.question ('O que você quer comprar?');
+let paraQue = readlineSync.question ('Qual o motivo da sua compra?');
+
+console.log ( 'Como' + quem + 'eu desejo comprar ' + oQue + paraQue  );
