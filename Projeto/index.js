ae7bab17d1fef1f0524efe518ca1d1aa6f375da2
Garcez0506 <fernando.garcez@acad.pucrs.br>
2022-08-19 16:54:59 -0300

    Codigo Finalizado

--- Projeto/index.js
+++ Projeto/index.js
@@ -2,7 +2,7 @@ import chalk from 'chalk';
 import readlineSync from 'readline-sync';
 
 let quem = readlineSync.question ('Quem quer comprar?');
-let oQue = readlineSync.question ('O que você quer comprar?');
+let oQue = readlineSync.question ('O que voce quer comprar?');
 let paraQue = readlineSync.question ('Qual o motivo da sua compra?');
 
-console.log ( 'Como' + (chalk.green(quem)) + 'eu desejo comprar ' + (chalk.red(oQue)) + (chalk.blue ( paraQue)  );
+console.log ( 'Como ' + (chalk.green(quem)) + ' ' + 'eu desejo comprar ' + (chalk.red(oQue)) + ' ' + 'Para ' + (chalk.blue ( paraQue)  ))
